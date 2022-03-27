dc070a1be7eab2bfcfbfc3a08f002a40437474b8
arebowicz <arebowicz@gmail.com>
2022-03-28 00:35:35 +0200

    Simple list algorithms

--- simple list algorithms/SLI.c
+++ simple list algorithms/SLI.c
@@ -1,5 +1,7 @@
 #include "list.h"
 
+static void listAction(list *);
+
 int main (int argc, char *argv[]) {
   list *l1 = listConstructor();
   list *l2 = listConstructor();
@@ -11,35 +13,20 @@ int main (int argc, char *argv[]) {
   strToList("1 2 3", l3);
   strToList("1 2 3 1 2 3 1 2 3", l4);
   strToList("1 2 3 3 3 3 33 3 3 3 3", l5);
-  printList(l1);
-  printMinimumAndItsPosition(l1);
-  reverseList(l1);
-  removeDuplicates(l1);
-  printList(l1);
-  deleteList(l1);
-  printList(l2);
-  printMinimumAndItsPosition(l2);
-  reverseList(l2);
-  removeDuplicates(l2);
-  printList(l2);
-  deleteList(l2);
-  printList(l3);
-  printMinimumAndItsPosition(l3);
-  reverseList(l3);
-  removeDuplicates(l3);
-  printList(l3);
-  deleteList(l3);
-  printList(l4);
-  printMinimumAndItsPosition(l4);
-  reverseList(l4);
-  removeDuplicates(l4);
-  printList(l4);
-  deleteList(l4);
-  printList(l5);
-  printMinimumAndItsPosition(l5);
-  reverseList(l5);
-  removeDuplicates(l5);
-  printList(l5);
-  deleteList(l5);
+  listAction(l1);
+  listAction(l2);
+  listAction(l3);
+  listAction(l4);
+  listAction(l5);
   return 0;
+}
+
+static void listAction(list *l) {
+  printList(l);
+  printMinimumAndItsPosition(l);
+  reverseList(l);
+  removeDuplicates(l);
+  printList(l);
+  deleteList(l);
+  printf("\n");
 }
